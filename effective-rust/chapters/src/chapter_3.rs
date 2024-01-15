87fdd12b8931ee12a30a8dba696d2300726318a9
d3adl0ck3 <keith.mcdade@me.com>
2024-01-15 12:17:37 -0700

    Add some more examples from effective rust

--- effective-rust/chapters/src/chapter_3.rs
+++ effective-rust/chapters/src/chapter_3.rs
@@ -6,6 +6,9 @@ pub fn all() {
     item_1::print_page_blah(true,false);
     item_1::print_page_now();
     item_1::enum_match();
+    item_2::function_pointers();
+    item_2::closures::try_it_out_without_closures();
+    item_2::closures::try_it_out_with_closures();
 }
 mod item_1 {
     pub fn mismatched_types() {
@@ -106,17 +109,266 @@ mod item_1 {
 }
 mod item_2 {
     enum Shape {
-    Rectangle { width: f64, height: f64 },
-    Circle { radius: f64 },
-}
+        Rectangle { width: f64, height: f64 },
+        Circle { radius: f64 },
+    }
 
-impl Shape {
-    pub fn area(&self) -> f64 {
-        match self {
-            Shape::Rectangle { width, height } => width * height,
-            Shape::Circle { radius } => std::f64::consts::PI * radius * radius,
+    impl Shape {
+        pub fn area(&self) -> f64 {
+            match self {
+                Shape::Rectangle { width, height } => width * height,
+                Shape::Circle { radius } => std::f64::consts::PI * radius * radius,
+            }
+        }
+    }
+    
+    fn sum(x: i32, y: i32) -> i32 {
+        x + y
+    }
+    pub fn function_pointers() {
+        let op: fn(i32, i32) -> i32 = sum;
+        let result = op(2,3);
+        println!("result is {result}");
+    }
+    pub mod closures {
+        // In real code, an `Iterator` method would be more appropriate.
+        // Should use the FnMut trait instead so it can accept closures
+        //fn modify_all(data: &mut [u32], mutator: fn(u32) -> u32) {
+        //    for value in data {
+        //        *value = mutator(*value);
+        //    }
+       // }
+        fn add2(v: u32) -> u32 {
+            v + 2
+        }
+        pub fn try_it_out_without_closures() {
+            let mut data = vec![1,2,3];
+            modify_all(&mut data, add2);
+            assert_eq!(data, vec![3, 4, 5,]);
+        }
+        pub fn try_it_out_with_closures() {
+            let amount_to_add = 3;
+            let add_n = |y| {
+                y + amount_to_add
+            };
+            let z = add_n(5);
+            println!("add_n of 5 = {z}");
+            assert_eq!(z, 8);
+        }
+        pub fn modify_all<F>(data: &mut [u32], mut mutator: F)
+            where
+            F: FnMut(u32) -> u32,
+            {
+                for value in data {
+                    *value = mutator(*value);
+                }
+            }
+    }
+    pub mod traits {
+        pub trait Sort {
+            fn sort(&mut self);
+        }
+        // Use marker traits to distinguish behaviors that cannot be expressed in the trait
+        // method siganture
+        pub trait StableSort: Sort {}
+    }
+    use std::fmt::Debug;
+    use crate::chapter_3::item_2::traits::Sort;
+    // Prefer accepting trait types to concrete types
+    // Use trait bounds to express requirements on the types used in generics
+    pub fn dump_sorted<T>(mut collection: T)
+        where
+        T: Sort + IntoIterator,
+        T::Item: Debug,
+        {
+            // Next line requires `T: Sort` trait bound.
+            collection.sort();
+            // Next line requires `T: IntoIterator` trait bound.
+            for item in collection {
+                // Next line requires `T::Item : Debug` trait bound
+                println!("{:?}", item);
+            }
+        }
+
+}
+mod item_3 {
+    // Avoid matching Option and Result
+    pub fn avoid_match() {
+        struct S {
+            field: Option<i32>,
+        }
+
+        let s = S { field: Some(42) };
+        match &s.field {
+            Some(i) => println!("field is {}", i),
+            None => {}
+        }
+        if let Some(i) = &s.field {
+            println!("field is {}", i);
+        }
+        let result = std::fs::File::open("/etc/passwd");
+        let f = match result {
+            Ok(f) => f,
+            Err(_e) => panic!("Failed to open /etc/passwd!"),
+        };
+        let f = std::fs::File::open("/etc/passwd").unwrap();
+    }
+
+    // prefer Result to Option
+    pub struct UserId;
+    pub fn find_user(username: &str) -> Result<UserId, std::io::Error> {
+        let f = match std::fs::File::open("/etc/passwd") {
+            Ok(f) => f,
+            Err(e) => return Err(e),
+        };
+        println!("Got me an f-ing f {:?}",f);
+        Ok(UserId{})
+    }
+    pub fn find_user_short(username: &str) -> Result<UserId, std::io::Error> {
+        let f = std::fs::File::open("/etc/passwd")?; 
+        println!("Got me an f-ing f {:?}",f);
+        Ok(UserId{})
+    }
+    pub fn find_user_remap(username: &str) -> Result<UserId, String> {
+        let f = std::fs::File::open("/etc/passwd")
+            .map_err(|e| format!("Failed to open password file: {:?}", e))?;
+        println!("Got me an f-ing f {:?}",f);
+        Ok(UserId{})
+    }
+    struct InputData {
+        payload: Option<Vec<u8>>,
+    }
+    impl InputData {
+    pub fn encrypted(&self) -> Vec<u8> {
+            self.payload.as_ref().unwrap_or(&vec![]).clone()
         }
     }
 }
+// Prefer idiomatic arror variants
+mod item_4 {
+    // Minimal Errors
+    pub struct UserId;
+    pub fn find_user(username: &str) -> Result<UserId, String> {
+        let f = std::fs::File::open("/etc/passwd")
+            .map_err(|e| format!("Failed to open password file: {:?}", e))?;
+        Ok(UserId{})
+    }
+    // We might like to impl Error for String{} but we can't because of Orphan ruloe
+    // Type alias doesn't help eaither since it is only an alias not a new type
+    // pub type MyError = String;
+    //impl std::error::Error for MyError {}
+    
 
+    #[derive(Debug)]
+    pub struct MyError(String);
+
+    impl std::fmt::Display for MyError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    impl std::error::Error for MyError {}
+
+    pub fn find_useri_too(username: &str) -> Result<UserId, MyError> {
+        let f = std::fs::File::open("/etc/passwd").map_err(|e| {
+            MyError(format!("Failed to open password file: {:?}", e))
+        })?;
+        Ok(UserId{})
+    }
+    // From<String> lets the compiler automatically apply any relevant From tain implementations
+    // needed to get to the desired type
+    impl std::convert::From<String> for MyError {
+        fn from(msg: String) -> Self {
+            Self(msg)
+        }
+    }
+    pub fn find_useri_trie(username: &str) -> Result<UserId, MyError> {
+        let f = std::fs::File::open("/etc/passwd")
+            .map_err(|e| format!("Failed to open password file: {:?}", e))?;
+        Ok(UserId{})
+    }
+    // Nested Errors
+    pub mod nested_errors { 
+        #[derive(Debug)]
+        pub enum MyError {
+            Io(std::io::Error),
+            Utf8(std::string::FromUtf8Error),
+            General(String),
+        }
+        impl std::fmt::Display for MyError {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    MyError::Io(e) => write!(f, "IO error: {}", e),
+                    MyError::Utf8(e) => write!(f, "UTF-8 error: {}", e),
+                    MyError::General(s) => write!(f, "General error: {}", s),
+                }
+            }
+        }
+        use std::error::Error;
+
+        impl Error for MyError {
+            fn source(&self) -> Option<&(dyn Error + 'static)> {
+                match self {
+                    MyError::Io(e) => Some(e),
+                    MyError::Utf8(e) => Some(e),
+                    MyError::General(_) => None,
+                }
+            }
+        }
+        impl From<std::io::Error> for MyError {
+            fn from(e: std::io::Error) -> Self {
+                Self::Io(e)
+            }
+        }
+        impl From<std::string::FromUtf8Error> for MyError {
+            fn from(e: std::string::FromUtf8Error) -> Self {
+                Self::Utf8(e)
+            }
+        }
+        const MAX_LEN: usize = 32;
+        use std::io::BufRead;
+        pub fn first_line(filename: &str) -> Result<String, MyError> {
+            let file = std::fs::File::open(filename)?; // via `From<std::io::Error>`
+            let mut reader = std::io::BufReader::new(file);
+            let mut buf = vec![];
+            let len = reader.read_until(b'\n', &mut buf)?; // via `From<std::io::Error>`
+            let result = String::from_utf8(buf)?; // via `From<std::string::FromUtf8Error>`
+            if result.len() > MAX_LEN {
+                return Err(MyError::General(format!("Line too long: {}", len)));
+            }
+            Ok(result)
+        }
+    }
+}
+pub mod item_5 {
+    pub mod copy {
+        pub fn copy_semantics(){
+            #[derive(Debug, Clone, Copy)]
+            struct KeyId(u32);
+            let k = KeyId(42);
+            let k2 = k; // value bitwise copied from k to k2
+            println!("k={:?}", k);
+        }
+    }
+    pub mod default {
+        #[derive(Default)]
+        struct Color {
+            red: u8,
+            green: u8,
+            blue: u8,
+            alpha: u8,
+        }
+        pub fn default_semantics() {
+            let c = Color {
+                red: 128,
+                ..Default::default()
+            }
+        }
+    }
+    // No example code, just see item I guess
+}
+// Understand type conversions
+pub mod item_6 {
+    
 }
